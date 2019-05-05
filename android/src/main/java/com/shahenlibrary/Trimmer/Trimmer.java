94592ab4d902433023bc6bd94c429178a981e300
yannoudou <19892810+yannoudou@users.noreply.github.com>
2019-05-05 15:42:41 +0200

    Update Trimmer.java
    update aspectRatio

--- android/src/main/java/com/shahenlibrary/Trimmer/Trimmer.java
+++ android/src/main/java/com/shahenlibrary/Trimmer/Trimmer.java
@@ -261,8 +261,8 @@ public class Trimmer {
       int height = Integer.parseInt(retriever.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
       int orientation = Integer.parseInt(retriever.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION));
 
-      float aspectRatio = width / height;
-
+      float aspectRatio = (float)width / (float)height;
+      
       int resizeWidth = 200;
       int resizeHeight = Math.round(resizeWidth / aspectRatio);
 
